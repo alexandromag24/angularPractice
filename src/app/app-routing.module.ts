bcbcfc84c4d97ef68cf7bab96a254f6ab6e78767
Alejandro Perez Rodriguez <alejandro.perez@vra.cubacatering.avianet.cu>
2020-09-25 13:11:05 -0400

    adding changes on forms

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { UserRegisterComponent } from './user/user-register/user-register.component';
+import { UserLoginComponent } from './user/user-login/user-login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AddPropertyComponent } from './property/add-property/add-property.component';
@@ -5,10 +7,13 @@ import { PropertyListComponent } from './property/property-list/property-list.co
 import { PropertyDetailComponent } from './property/property-detail/property-detail.component';
 
 
+
 const routes: Routes = [
   {path: '', component: PropertyListComponent},
   {path: 'rent-property', component: PropertyListComponent},
   {path: 'add-property', component: AddPropertyComponent},
+  {path: 'user-login', component: UserLoginComponent},
+  {path: 'user-register', component: UserRegisterComponent},
   {path: 'property-detail/:id', component: PropertyDetailComponent},
   {path: '**', component: PropertyListComponent}
 ];
--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertifyService } from '../services/alertify.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
-
-  constructor() { }
+  loggedinUser: string;
+  constructor(private alertify: AlertifyService) { }
 
   ngOnInit() {
+  }
+
+  loggedIn(){
+    this.loggedinUser = localStorage.getItem('token');
+    return this.loggedinUser;
+  }
+  onLogout(){
+    localStorage.removeItem('token');
+    this.alertify.success('You are logged out !');
   }
 
 }
